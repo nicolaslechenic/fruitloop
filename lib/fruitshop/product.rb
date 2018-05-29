dbbc23d687d593f7e52dd0ec8f469d852999faa4
Nicolas Zermati <nicoolas25@gmail.com>
2018-05-29 06:22:52 +0200

    Use a Struct as a value object rather than a plain class
    The struct will handle the `#==`, `#eql?`, and `#hash` methods. Because
    we don't expect mutations, the `#unique_name=` method is removed.

--- lib/fruitshop/product.rb
+++ lib/fruitshop/product.rb
@@ -1,11 +1,9 @@
 module Fruitshop
-  class Product
-    def initialize(uniq_name)
-      @uniq_name = uniq_name
-    end
+  Product = Struct.new(:unique_name) do
+    remove_method :unique_name=
 
     def price
-      case @uniq_name
+      case unique_name
       when 'Pommes'
         100
       when 'Cerises'
@@ -18,24 +16,11 @@ module Fruitshop
     end
 
     def discount(per_product_counter)
-      if @uniq_name == 'Cerises' && per_product_counter[self].even?
+      if unique_name == 'Cerises' && per_product_counter[self].even?
         - 20
       else
         0
       end
     end
-
-    def ==(other)
-      self.class === other && other.uniq_name == @uniq_name
-    end
-
-    alias eql? ==
-
-    def hash
-      @uniq_name.hash
-    end
-
-    attr_reader :uniq_name
-    protected :uniq_name
   end
 end
